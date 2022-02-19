4fb92cd306f2a622644668531e5e742e46dee341
Shengjie Xu <shengjie.xu@mail.utoronto.ca>
2022-02-18 22:05:28 -0500

    More stub code on input model

--- src/preppipe/inputmodel.py
+++ src/preppipe/inputmodel.py
@@ -31,7 +31,8 @@
 # @~chinese @brief 请从 preppipe.inputmodel 查看文档
 # @~
 
-from tabnanny import check
+# from tabnanny import check
+# from tkinter.font import names
 import typing
 import PIL.Image
 import importlib
@@ -140,48 +141,65 @@ class IMTableBlock(IMBlock):
 class IMDocument(IMBlockList):
   local_options: typing.Dict[str, typing.Any]
 
-
-class InputModel(IMBase):
-  global_options: typing.Dict[str, typing.Any]
+class IMNamespace(IMBase):
+  # namespace: stand-alone package; only the global namespace (namespace = []) is always present.
+  # InputModel cannot assume namespace-d assets are always available.
+  namespace: typing.List[str]
+  options: typing.Dict[str, typing.Any] # parsed from preppipe.json
   fileset: typing.Dict[str, IMDocument]
   asset_symbol_table : typing.List[IMAssetSymbolEntry]
   asset_path_dict : typing.Dict[str, IMAssetReference]
-  
+  asset_basepath_dict : typing.Dict[str, typing.List[str]]
   # in the input model, we have not yet resolved any "asset name"; assets are only referenced by path
   # asset_path_dict maps from asset path to the concrete asset symbol entry (for the ones with backing store only)
-  # we ensure that all files can be referenced starting from a common base path, specified by the config / command line
-  # (which means no need to remap out-of-tree asset directories to in-tree ones; out-of-tree assets are imported as if they are in-tree. users use in-tree path only)
-  # if we have path/to/file1.xxx, if there is no other file with the same basename in that directory, then we will have two entries in the asset_path_dict:
-  # path/to/file1.xxx -> ref
-  # path/to/file1     -> ref
-  # however, if we have path/to/file1.yyy as well, then we only
+  # we ensure that all files can be referenced by <namespace> + <path>
+  # no "out-of-tree" asset; they are either remapped to in-tree path or is a separate namespace
+  # for each file like path/to/file1.xxx:
+  # asset_path_dict['path/to/file1.xxx'] = <Asset>
+  # asset_basepath_dict['path/to/file1'] = ['xxx']
+  # if there is also path/to/file1.yyy, then
+  # asset_path_dict['path/to/file1.xxx'] = <Asset>
+  # asset_path_dict['path/to/file1.yyy'] = <Asset>
+  # asset_basepath_dict['path/to/file1'] = ['xxx', 'yyy']
+  # anonymous assets have empty path and does not have entries in asset_path_dict
   
-  
-  def __init__(self) -> None:
-    self.global_options = {}
+  def __init__(self, namespace : typing.List[str]) -> None:
+    self.namespace = namespace
+    self.options = {}
     self.fileset = {}
     self.asset_symbol_table = []
     self.asset_path_dict = {}
-
-class InputModelBuilder:
-  result : InputModel
-  hash_algorithm : str
-  asset_uniquing_dict_hash = typing.Dict[bytes, IMAssetReference]
-  asset_uniquing_dict_storepath = typing.Dict[str, IMAssetReference]
+    self.asset_basepath_dict = {}
   
-  # when we build the input model, we use (absolute) "path" for uniquing assets. We assume assets do not change during building.
-  # after the input model is built, assets can be referenced by "name", which is a relative or absolute path at this moment
 
-  def __init__(self, hash_algorithm : str = "sha512") -> None:
-    self.result = InputModel()
-    self.hash_algorithm = hash_algorithm
-    self.asset_uniquing_dict_hash = {}
-    self.asset_uniquing_dict_storepath = {}
+class InputModel(IMBase):
+  global_options: typing.Dict[str, typing.Any]
+  namespaces : typing.Dict[typing.List[str], IMNamespace]
   
-  def get_asset_entry_from_path(self, path : str) -> IMAssetSymbolEntry:
-    assert path 
+  def __init__(self) -> None:
+    self.global_options = {}
+    self.namespaces = {}
 
+class IMNamespaceBuilder:
+  _hash_algorithm : typing.ClassVar[str] = "sha512"
   
+  @staticmethod
+  def set_hash_algorithm(algorithm : str):
+    IMNamespaceBuilder._hash_algorithm = algorithm
+  
+  @staticmethod
+  def get_hash(snapshot : bytes) -> bytes:
+    hasher = hashlib.new(IMNamespaceBuilder._hash_algorithm)
+    hasher.update(snapshot)
+    return hasher.digest()
+  
+  # we use preppipe.json as configuration file format
+  # we may even use this function outside building IMNamespace, e.g. parse the global options
+  @staticmethod
+  def parse_configuration_file(f) -> typing.Dict[str, typing.Any]:
+    pass
+  
+  # ============================================================================
   class AssetInfo:
     # one for each asset
     asset : IMAssetSymbolEntry
@@ -192,25 +210,29 @@ class InputModelBuilder:
       self.asset = asset
       self.checksum = checksum
       self.name = name
-      
-  result : InputModel
-  assets_data : typing.List[AssetInfo]
-  anonymous_asset_uniquing_dict : typing.Dict[bytes, int] # key is a hash of asset; we may change the hash algorithm later on
-  stored_asset_uniquing_dict : typing.Dict[str, int] # backing store file absolute path -> index
-  hash_algorithm : str
   
-  def __init__(self, hash_algorithm : str = "sha512") -> None:
-    self.result = InputModel()
-    self.assets_data = []
-    self.anonymous_asset_uniquing_dict = {}
-    self.hash_algorithm = hash_algorithm
+  _result: IMNamespace
+  _assets_data : typing.List[AssetInfo]
+  _anonymous_asset_uniquing_dict : typing.Dict[bytes, int] # key is a hash of asset; we may change the hash algorithm later on
+  _stored_asset_uniquing_dict : typing.Dict[str, int] # backing store file absolute path -> index
   
-  def get_hash(self, snapshot : bytes) -> bytes:
-    hasher = hashlib.new(self.hash_algorithm)
-    hasher.update(snapshot)
-    return hasher.digest()
+  # when we build namespaces, we use (absolute) "path" for uniquing assets. We assume assets do not change during building.
+  # after the input model is built, assets can be referenced by "name", which is a relative or absolute path at this moment
+  
+  def get_result(self) -> IMNamespace:
+    pass
+  
+  def get_asset_entry_from_path(self, path : str) -> IMAssetSymbolEntry:
+    assert path 
+  
+  def __init__(self) -> None:
+    self._result = IMNamespace()
+    self._assets_data = []
+    self._anonymous_asset_uniquing_dict = {}
+    self._stored_asset_uniquing_dict = {}
   
   def try_get_asset_info(self, *, store_path : str = "", checksum : bytes = None) -> AssetInfo:
+    # TODO To be updated
     if len(store_path) > 0:
       # search for duplicates according to store path
       if store_path in self.stored_asset_uniquing_dict:
@@ -222,6 +244,7 @@ class InputModelBuilder:
     return None
     
   def add_asset(self, asset : AssetBase, name : str) -> None:
+    # TODO To be updated
     # the caller must ensure the asset is not duplicated
     # step 1: try to prove no duplicates
     
@@ -234,7 +257,5 @@ class InputModelBuilder:
       # just register and done
       asset_index = len(self.assets_data)
       # asset_info = AssetInfo(asset, checksum, name)
-      
-      
     pass
     
