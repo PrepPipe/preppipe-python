21208316e40ea1cbad13f3c2c480210b285c3098
Shengjie Xu <xsj617603321@gmail.com>
2023-05-13 09:30:45 +0800

    More code for RenPy AST export

--- src/preppipe/renpy/ast.py
+++ src/preppipe/renpy/ast.py
@@ -22,7 +22,13 @@ class RenPyNode(Operation):
 
 @irdataop.IROperationDataclassWithValue(VoidType)
 class RenPyASMNode(RenPyNode, Value):
+  # 可以多行的 ASM
   asm : OpOperand[StringListLiteral] # 带一个 StringListLiteral，每行一个字符串，行首空格保留（只会有空格，RenPy不支持Tab缩进），行末尾没有\r\n这种EOL
+
+  def get_singleline_str(self) -> str:
+    if sl := self.asm.try_get_value():
+      return ','.join(sl.value)
+    return ''
 
   @staticmethod
   def create(context : Context, asm : StringLiteral | StringListLiteral, name : str = '', loc : Location | None = None) -> RenPyASMNode:
@@ -30,6 +36,17 @@ class RenPyASMNode(RenPyNode, Value):
       asm = StringListLiteral.get(context, (asm,))
     return RenPyASMNode(init_mode=IRObjectInitMode.CONSTRUCT, context=context, asm=asm, name=name, loc=loc)
 
+@irdataop.IROperationDataclassWithValue(VoidType)
+class RenPyASMExpr(RenPyNode, Value):
+  # 单行或内嵌的 ASM
+  asm : OpOperand[StringLiteral]
+
+  def get_string(self) -> str:
+    return self.asm.get().get_string()
+
+  @staticmethod
+  def create(context : Context, asm : StringLiteral | str, name : str = '', loc : Location | None = None) -> RenPyASMExpr:
+    return RenPyASMExpr(init_mode=IRObjectInitMode.CONSTRUCT, context=context, asm=asm, name=name, loc=loc)
 
 @irdataop.IROperationDataclassWithValue(VoidType)
 class RenPyCharacterExpr(RenPyNode, Value):
@@ -41,19 +58,19 @@ class RenPyCharacterExpr(RenPyNode, Value):
   what_suffix : OpOperand[StringLiteral]
   who_prefix  : OpOperand[StringLiteral]
   who_suffix  : OpOperand[StringLiteral]
-  dynamic   : OpOperand[RenPyASMNode] # 取一个从 RenPyASMNode 来的值
-  condition : OpOperand[RenPyASMNode] # 取一个从 RenPyASMNode 来的值
+  dynamic   : OpOperand[RenPyASMExpr]
+  condition : OpOperand[RenPyASMExpr]
   interact  : OpOperand[BoolLiteral] # bool，是否等待用户点击
   advance   : OpOperand[BoolLiteral] # bool, 用户是否可以通过点击来继续。（不行的话可能必须要点一个链接或是执行其他操作来继续）
   mode      : OpOperand[StringLiteral] # str: https://www.renpy.org/doc/html/modes.html#modes
   callback  : OpOperand[StringLiteral] # str, 一个函数名
-  ctc             : OpOperand[RenPyASMNode] # RenPyASMNode, displayable expr
-  ctc_pause       : OpOperand[RenPyASMNode]
-  ctc_timedpause  : OpOperand[RenPyASMNode]
+  ctc             : OpOperand[RenPyASMExpr] # displayable expr
+  ctc_pause       : OpOperand[RenPyASMExpr]
+  ctc_timedpause  : OpOperand[RenPyASMExpr]
   ctc_position    : OpOperand[StringLiteral] # str : enum {"nestled", "nestled-close", "fixed"}
   screen      : OpOperand[StringLiteral] # str : 用来显示对话的 Screen 的名称
-  show_params : OpOperand[StringListLiteral] # RenPyASMNode，应该是一串 "show_param1=xxx" "show_param2=yyy" 这样的参数
-  body        : Block # 放需要的 RenPyASMNode
+  show_params : OpOperand[RenPyASMExpr] # 应该是一串 "show_param1=xxx" "show_param2=yyy" 这样的参数
+  body        : Block # 放需要的 RenPyASMExpr
 
   @staticmethod
   def create(context : Context, displayname : StringLiteral | str) -> RenPyCharacterExpr:
@@ -66,16 +83,16 @@ class RenPyDefineNode(RenPyNode, Value):
   # store : str = 'store'
   # varname : str
   # operator : enum: '=', '+=', '|='
-  # expr : RenPyCharacterExpr / RenPyImageExpr / RenPyASMNode
+  # expr : RenPyCharacterExpr / RenPyImageExpr / RenPyASMExpr
   store : OpOperand[StringLiteral]
   varname : OpOperand[StringLiteral]
   assign_operator : OpOperand[StringLiteral] # enum: '=', '+=', '|='
-  expr : OpOperand[Value] # RenPyCharacterExpr / RenPyImageExpr / RenPyASMNode
+  expr : OpOperand[Value] # RenPyCharacterExpr / RenPyImageExpr / RenPyASMExpr
   body : Block
 
   @staticmethod
-  def create(context : Context, varname : StringLiteral | str, expr : Value) -> RenPyDefineNode:
-    return RenPyDefineNode(init_mode=IRObjectInitMode.CONSTRUCT, context=context, varname=varname, expr=expr)
+  def create(context : Context, varname : StringLiteral | str, expr : Value, store : StringLiteral | str | None = None, assign_operator : StringLiteral | str | None = None) -> RenPyDefineNode:
+    return RenPyDefineNode(init_mode=IRObjectInitMode.CONSTRUCT, context=context, varname=varname, expr=expr, store=store, assign_operator=assign_operator)
 
   @staticmethod
   def create_character(context : Context, varname : StringLiteral | str, displayname : StringLiteral | str) -> tuple[RenPyDefineNode, RenPyCharacterExpr]:
@@ -89,6 +106,30 @@ class RenPyDefineNode(RenPyNode, Value):
     if s := self.store.try_get_value():
       return s.get_string() + '.' + vname
     return vname
+
+@irdataop.IROperationDataclassWithValue(VoidType)
+class RenPyDefaultNode(RenPyNode, Value):
+  # 用来给变量做声明、赋初值
+  # store : str = 'store'
+  # varname : str
+  # expr
+  store : OpOperand[StringLiteral]
+  varname : OpOperand[StringLiteral]
+  expr : OpOperand[RenPyASMExpr]
+  body : Block
+
+  def get_varname_str(self) -> str:
+    vname = self.varname.get().get_string()
+    if s := self.store.try_get_value():
+      return s.get_string() + '.' + vname
+    return vname
+
+  @staticmethod
+  def create(context : Context, varname : StringLiteral | str, expr : RenPyASMExpr, store : StringLiteral | str | None = None) -> RenPyDefaultNode:
+    result = RenPyDefaultNode(init_mode=IRObjectInitMode.CONSTRUCT, context=context, varname=varname, expr=expr, store=store)
+    result.body.push_back(expr)
+    return result
+
 
 @irdataop.IROperationDataclass
 class RenPySayNode(RenPyNode):
@@ -118,10 +159,20 @@ class RenPySayNode(RenPyNode):
 class RenPyInitNode(RenPyNode):
   # RenPy 的 init 块
   # 除了优先级外基本上只有个 body
+  # 如果是单纯的 init, 则 pythoncode 为空，所有 RenPy AST结点都在 body 内
+  # 如果是 init pyton 块，则 pythoncode 包含所有 python 代码，该 RenPyASMNode 储存在 body 中
   # priority : int
   # body : region
   priority : OpOperand[IntLiteral]
+  pythoncode : OpOperand[RenPyASMNode]
   body : Block
+
+  @staticmethod
+  def create(context : Context, priority : IntLiteral | int, pythoncode : RenPyASMNode | None = None):
+    result = RenPyInitNode(init_mode=IRObjectInitMode.CONSTRUCT, context=context, priority=priority, pythoncode=pythoncode)
+    if pythoncode is not None:
+      result.body.push_back(pythoncode)
+    return result
 
 @irdataop.IROperationDataclass
 class RenPyLabelNode(RenPyNode):
@@ -146,40 +197,91 @@ class RenPyPythonNode(RenPyNode):
   # store : str = 'store' # 使用哪个 store 对象来存值，暂不支持
   code : OpOperand[RenPyASMNode]
   hide : OpOperand[BoolLiteral]
+  store : OpOperand[StringLiteral] # in xxx
   body : Block
+
+  @staticmethod
+  def create(context : Context, code : RenPyASMNode, hide : BoolLiteral | bool | None = None, store : StringLiteral | str | None = None):
+    result = RenPyPythonNode(init_mode=IRObjectInitMode.CONSTRUCT, context=context, code=code, hide=hide, store=store)
+    result.body.push_back(code)
+    return result
 
 @irdataop.IROperationDataclass
 class RenPyEarlyPythonNode(RenPyPythonNode):
-  pass
+
+  @staticmethod
+  def create(context : Context, code : RenPyASMNode):
+    result = RenPyEarlyPythonNode(init_mode=IRObjectInitMode.CONSTRUCT, context=context, code=code)
+    result.body.push_back(code)
+    return result
 
 @irdataop.IROperationDataclass
 class RenPyImageNode(RenPyNode):
   #name : str
   #displayable : str # atl or PyCode
   codename : OpOperand[StringLiteral]
-  displayable : OpOperand[RenPyASMNode]
+  displayable : OpOperand[RenPyASMExpr]
   body : Block
 
-@irdataop.IROperationDataclass
-class RenPyTransformNode(RenPyNode):
+  @staticmethod
+  def create(context : Context, codename : typing.Iterable[str] | StringLiteral | str, displayable : RenPyASMExpr | StringLiteral | str):
+    if isinstance(displayable, str):
+      displayable = StringLiteral.get(displayable, context)
+    if isinstance(displayable, StringLiteral):
+      displayable = RenPyASMExpr.create(context, displayable)
+    assert isinstance(displayable, RenPyASMExpr)
+    result = RenPyImageNode(init_mode=IRObjectInitMode.CONSTRUCT, context=context, codename=codename, displayable=displayable)
+    result.body.push_back(displayable)
+    return result
+
+#@irdataop.IROperationDataclass
+#class RenPyTransformNode(RenPyNode):
   # store : str = 'store'
   # name : str
   # atl : ATL code block
   # parameters : 应该表述为类似 inspect.signature 的对象
-  codename : OpOperand[StringLiteral]
-  store : OpOperand[StringLiteral]
-  parameters : OpOperand[StringLiteral]
-  atl : OpOperand[RenPyASMNode]
-  body : Block
+#  codename : OpOperand[StringLiteral]
+#  store : OpOperand[StringLiteral]
+#  parameters : OpOperand[StringLiteral]
+#  atl : OpOperand[RenPyASMNode]
+#  body : Block
+
+@irdataop.IROperationDataclassWithValue(VoidType)
+class RenPyWithNode(RenPyNode, Value):
+  # expr : str
+  # paired: 暂时不管
+  expr : OpOperand[StringLiteral]
+
+  @staticmethod
+  def create(context : Context, expr : typing.Iterable[StringLiteral | str] | StringLiteral | str):
+    return RenPyWithNode(init_mode=IRObjectInitMode.CONSTRUCT, context=context, expr=expr)
 
 @irdataop.IROperationDataclass
 class RenPyShowNode(RenPyNode):
   # imspec: list[str]
   # atl
-  imspec : OpOperand[StringListLiteral]
-  atl : OpOperand[RenPyASMNode] # at block
-  body : Block # only for ATL
+  imspec : OpOperand[StringLiteral]
+  showas : OpOperand[StringLiteral]
+  atl : OpOperand[RenPyASMExpr] # at block
   behind : OpOperand[StringLiteral] # 0-N 个图片名
+  onlayer : OpOperand[StringLiteral]
+  zorder : OpOperand[IntLiteral]
+  with_ : OpOperand[RenPyWithNode]
+  body : Block # only for ATL
+
+  @staticmethod
+  def create(context : Context, imspec : typing.Iterable[StringLiteral | str] | StringLiteral | str,
+             showas : StringLiteral | str | None = None, showat : RenPyASMExpr | None = None,
+             behind : typing.Iterable[StringLiteral | str] | StringLiteral | str | None = None,
+             onlayer : StringLiteral | str | None = None,
+             zorder : IntLiteral | int | None = None,
+             with_ : RenPyWithNode | None = None):
+    result = RenPyShowNode(init_mode=IRObjectInitMode.CONSTRUCT, context=context, imspec=imspec, showas=showas, atl=showat, behind=behind, onlayer=onlayer, zorder=zorder, with_=with_)
+    if showat is not None:
+      result.body.push_back(showat)
+    if with_ is not None:
+      result.body.push_back(with_)
+    return result
 
 #class RenPyShowLayerNode(RenPyNode):
   # 可以暂时不做
@@ -201,21 +303,33 @@ class RenPySceneNode(RenPyNode):
   # 'imspec' : list[str]
   # 'layer' : str
   # 'atl',
-  imspec : OpOperand[StringListLiteral]
-  layer : OpOperand[StringLiteral]
-  atl : OpOperand[RenPyASMNode]
+  imspec : OpOperand[StringLiteral]
+  #layer : OpOperand[StringLiteral]
+  #atl : OpOperand[RenPyASMExpr]
+  with_ : OpOperand[RenPyWithNode]
   body : Block # only for ATL
+
+  @staticmethod
+  def create(context : Context, imspec : typing.Iterable[StringLiteral | str] | StringLiteral | str, with_ : RenPyWithNode | None = None):
+    result = RenPySceneNode(init_mode=IRObjectInitMode.CONSTRUCT, context=context, imspec=imspec, with_=with_)
+    if with_ is not None:
+      result.body.push_back(with_)
+    return result
 
 @irdataop.IROperationDataclass
 class RenPyHideNode(RenPyNode):
   # 'imspec' : list[str]
-  imspec : OpOperand[StringListLiteral]
+  imspec : OpOperand[StringLiteral]
+  onlayer : OpOperand[StringLiteral]
+  with_ : OpOperand[RenPyWithNode]
+  body : Block
 
-@irdataop.IROperationDataclass
-class RenPyWithNode(RenPyNode):
-  # expr : str
-  # paired: 暂时不管
-  expr : OpOperand[StringLiteral]
+  @staticmethod
+  def create(context : Context, imspec : typing.Iterable[StringLiteral | str] | StringLiteral | str, onlayer : StringLiteral | str | None = None, with_ : RenPyWithNode | None = None):
+    result = RenPyHideNode(init_mode=IRObjectInitMode.CONSTRUCT, context=context, imspec=imspec, onlayer=onlayer, with_=with_)
+    if with_ is not None:
+      result.body.push_back(with_)
+    return result
 
 @irdataop.IROperationDataclass
 class RenPyCallNode(RenPyNode):
@@ -225,20 +339,42 @@ class RenPyCallNode(RenPyNode):
   label : OpOperand[StringLiteral]
   is_expr : OpOperand[BoolLiteral] # label 是否是表达式
 
+  @staticmethod
+  def create(context : Context, label : StringLiteral | str, is_expr : BoolLiteral | bool | None = None):
+    return RenPyCallNode(init_mode=IRObjectInitMode.CONSTRUCT, context=context, label=label, is_expr=is_expr)
+
 @irdataop.IROperationDataclass
 class RenPyReturnNode(RenPyNode):
   # expr : str
-  expr : OpOperand[StringLiteral]
+  expr : OpOperand[RenPyASMExpr]
+  body : Block
+
+  @staticmethod
+  def create(context: Context, expr : RenPyASMExpr | None = None):
+    result = RenPyReturnNode(init_mode=IRObjectInitMode.CONSTRUCT, context=context, expr=expr)
+    if expr is not None:
+      result.body.push_back(expr)
+    return result
 
 @irdataop.IROperationDataclass
 class RenPyMenuItemNode(RenPyNode):
   # 用于 menu 命令
   # "<label>" (<arguments>) if <condition>:
   #    <body>
-  label : OpOperand[TextLiteral]
-  condition : OpOperand[StringLiteral]
-  arguments : OpOperand[StringLiteral]
+  label : OpOperand[Value]
+  condition : OpOperand[RenPyASMExpr]
+  arguments : OpOperand[RenPyASMExpr]
+  optionblock : Block
   body : Block
+
+  @staticmethod
+  def create(context: Context, label : typing.Iterable[TextFragmentLiteral | StringLiteral | str] | TextFragmentLiteral | StringLiteral | str, condition : RenPyASMExpr | None = None, arguments : RenPyASMExpr | None = None):
+    result = RenPyMenuItemNode(init_mode=IRObjectInitMode.CONSTRUCT, context=context, label=label, condition=condition, arguments=arguments)
+    if condition is not None:
+      result.optionblock.push_back(condition)
+    if arguments is not None:
+      result.optionblock.push_back(arguments)
+    return result
 
 @irdataop.IROperationDataclass
 class RenPyMenuNode(RenPyNode):
@@ -247,9 +383,18 @@ class RenPyMenuNode(RenPyNode):
   # set : str (执行选单时如果某项的标题已在 set 中存在，则该选项不会出现。用这个来实现“排除选项”的功能)
   # with_: with 从句, 暂时不管
   # has_caption: bool, 暂时不管
+  varname : OpOperand[StringLiteral]
   menuset : OpOperand[StringLiteral]
-  arguments : OpOperand[StringLiteral]
-  items : Block # 里面应该都是 RenPyMenuItemNode
+  arguments : OpOperand[RenPyASMExpr]
+  optionblock : Block
+  items : Block # 里面有 0-1个 RenPySayNode ，其余应该都是 RenPyMenuItemNode
+
+  @staticmethod
+  def create(context : Context, varname : StringLiteral | str | None = None, menuset : StringLiteral | str | None = None, arguments : RenPyASMExpr | None = None):
+    result = RenPyMenuNode(init_mode=IRObjectInitMode.CONSTRUCT, context=context, varname=varname, menuset=menuset, arguments=arguments)
+    if arguments is not None:
+      result.optionblock.push_back(arguments)
+    return result
 
 @irdataop.IROperationDataclass
 class RenPyJumpNode(RenPyNode):
@@ -258,42 +403,53 @@ class RenPyJumpNode(RenPyNode):
   target : OpOperand[StringLiteral]
   is_expr : OpOperand[BoolLiteral] # target是否是表达式
 
+  @staticmethod
+  def create(context : Context, target : StringLiteral | str, is_expr : BoolLiteral | bool | None = None):
+    return RenPyJumpNode(init_mode=IRObjectInitMode.CONSTRUCT, context=context, target=target, is_expr=is_expr)
+
 @irdataop.IROperationDataclass
 class RenPyPassNode(RenPyNode):
-  pass
+
+  @staticmethod
+  def create(context : Context):
+    return RenPyPassNode(init_mode=IRObjectInitMode.CONSTRUCT, context=context)
 
 @irdataop.IROperationDataclass
 class RenPyCondBodyPair(RenPyNode):
   # 用于 if 和 while
-  condition : OpOperand[StringLiteral]
+  condition : OpOperand[RenPyASMExpr]
   body : Block
 
+  # 不应直接创建，没有 create()
 
 @irdataop.IROperationDataclass
 class RenPyWhileNode(RenPyCondBodyPair):
   # condition : bool expr
   # block
-  pass
+  optionblock : Block # 存放 RenPyASMExpr
+
+  @staticmethod
+  def create(context : Context, condition : RenPyASMExpr):
+    result = RenPyWhileNode(init_mode=IRObjectInitMode.CONSTRUCT, context=context, condition=condition)
+    result.optionblock.push_back(condition)
+    return result
 
 @irdataop.IROperationDataclass
 class RenPyIfNode(RenPyNode):
   # entries : list[tuple[condition : bool expr, block]]
   entries : Block # 里面应该是一串 RenPyCondBodyPair, 按照判断顺序排列
+  optionblock : Block # 存放 RenPyASMExpr
 
+  def add_branch(self, condition : RenPyASMExpr | None) -> Block:
+    if condition is not None:
+      self.optionblock.push_back(condition)
+    branch = RenPyCondBodyPair(init_mode=IRObjectInitMode.CONSTRUCT, context=self.context, condition=condition)
+    self.entries.push_back(branch)
+    return branch.body
 
-
-@irdataop.IROperationDataclass
-class RenPyDefaultNode(RenPyNode):
-  # 用来给变量做声明、赋初值
-  # store : str = 'store'
-  # varname : str
-  # expr
-  store : OpOperand[StringLiteral]
-  varname : OpOperand[StringLiteral]
-  expr : OpOperand[StringLiteral]
-  body : Block
-
-# 其他像 Screen 什么的先不做
+  @staticmethod
+  def create(context : Context):
+    return RenPyIfNode(init_mode=IRObjectInitMode.CONSTRUCT, context=context)
 
 @irdataop.IROperationDataclass
 class RenPyFileAssetOp(Symbol):
@@ -340,6 +496,8 @@ class RenPyASTVisitor:
 
   def visitRenPyASMNode(self, v : RenPyASMNode):
     return self.visitChildren(v)
+  def visitRenPyASMExpr(self, v : RenPyASMExpr):
+    return self.visitChildren(v)
   def visitRenPyCharacterExpr(self, v : RenPyCharacterExpr):
     return self.visitChildren(v)
   def visitRenPyDefineNode(self, v : RenPyDefineNode):
@@ -356,8 +514,8 @@ class RenPyASTVisitor:
     return self.visitChildren(v)
   def visitRenPyImageNode(self, v : RenPyImageNode):
     return self.visitChildren(v)
-  def visitRenPyTransformNode(self, v : RenPyTransformNode):
-    return self.visitChildren(v)
+#  def visitRenPyTransformNode(self, v : RenPyTransformNode):
+#    return self.visitChildren(v)
   def visitRenPyShowNode(self, v : RenPyShowNode):
     return self.visitChildren(v)
   def visitRenPySceneNode(self, v : RenPySceneNode):
