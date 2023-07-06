fa6f7cf73d677deb057dc33526219032c0cd92c9
Shengjie Xu <xsj617603321@gmail.com>
2023-07-06 20:01:40 +0800

    Bugfix; added long say splitting pass

--- src/preppipe/transform/vnmodel/__init__.py
+++ src/preppipe/transform/vnmodel/__init__.py
@@ -1,5 +1,2 @@
 # SPDX-FileCopyrightText: 2023 PrepPipe's Contributors
 # SPDX-License-Identifier: Apache-2.0
-
-from .vnmodel import passes
-
--- src/preppipe/transform/vnmodel/passes.py
+++ src/preppipe/transform/vnmodel/passes.py
@@ -1,5 +1,5 @@
 # SPDX-FileCopyrightText: 2023 PrepPipe's Contributors
 # SPDX-License-Identifier: Apache-2.0
 
-from .vnmodel import passes
-
+from . import vnentryinference
+from . import vnlongsaysplitting
