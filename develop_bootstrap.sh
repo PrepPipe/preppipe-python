05732861cc52dcfec2bf03cfc7ea4b7f26cf7d8d
Shengjie Xu <xsj617603321@gmail.com>
2023-11-24 22:00:58 -0500

    Added tooling main() for imagepack

--- develop_bootstrap.sh
+++ develop_bootstrap.sh
@@ -6,7 +6,7 @@ sudo apt install -y python3-pip python3-venv ffmpeg
 # dependendies for building
 sudo python3 -m pip install build twine
 # dependencies for preppipe
-sudo python3 -m pip install llist odfpy python-docx chardet marko pillow pydub numpy scipy matplotlib opencv-python ghostscript editdistance bidict pypinyin graphviz
+sudo python3 -m pip install llist odfpy python-docx chardet marko pyyaml pillow pydub numpy scipy matplotlib opencv-python ghostscript editdistance bidict pypinyin graphviz
 sudo python3 -m pip install antlr4-python3-runtime>=4.10
 
 # do a build to generate src/preppipe/_version.py
