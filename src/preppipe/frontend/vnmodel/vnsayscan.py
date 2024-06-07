a01747a4ba07f4a253a512a7445cc2bd6899f923
Shengjie Xu <xsj617603321@gmail.com>
2024-06-07 17:26:23 -0400

    Bugfixes and minor improvements on say scanning

--- src/preppipe/frontend/vnmodel/vnsayscan.py
+++ src/preppipe/frontend/vnmodel/vnsayscan.py
@@ -31,6 +31,11 @@ class SayScanFieldPosition:
   start : int
   end : int
   text : str
+  # 英语中我们可能在引号内不加空格、在引号外加空格，所以像这样的语句：
+  # Yuhan: "Sentence 3", ignored, "continued."
+  # 说话内容就会成为 "Sentence 3continued."
+  # 当发现这类情况是，我们看原文中在引号后的字符（除了标点符号外）是否是空格，如果是的话我们就在这里加上一个空格
+  flag_append_space : bool = False
 
   def __str__(self) -> str:
     return '[' + str(self.start) + ',' + str(self.end) + ')"' + self.text + '"'
@@ -58,10 +63,12 @@ class SayScanResult:
 
 class SayScanner(SayScanVisitor):
   result : SayScanResult
+  rawtext : str
 
-  def __init__(self) -> None:
+  def __init__(self, rawtext : str) -> None:
     super().__init__()
     self.result = SayScanResult()
+    self.rawtext = rawtext
 
   def handle_quoted_str(self, node : antlr4.TerminalNode) -> SayScanFieldPosition:
     quoted_str_token = node.getSymbol()
@@ -94,12 +101,41 @@ class SayScanner(SayScanVisitor):
     text = node.getText()
     return SayScanFieldPosition(start, end, text)
 
+  def trim_text(self, field : SayScanFieldPosition, trim_start : bool = True, trim_end : bool = True) -> SayScanFieldPosition:
+    # 原地调整 field 的 start 和 end 以去掉前后的空白
+    is_changed = False
+    if trim_start:
+      while field.start < field.end and self.rawtext[field.start].isspace():
+        field.start += 1
+        is_changed = True
+    if trim_end:
+      while field.start < field.end and self.rawtext[field.end-1].isspace():
+        field.end -= 1
+        is_changed = True
+    if is_changed:
+      field.text = self.rawtext[field.start:field.end]
+    return field
+
+  def should_add_whitespace_after_quoted_str(self, stop : int) -> bool:
+    # 目前我们只考虑英语的情况，所以只检查 ASCII 字符
+    curpos : int = stop + 1 # 跳过引号
+    while curpos < len(self.rawtext):
+      nextchar = self.rawtext[curpos]
+      if nextchar == ' ':
+        return True
+      elif nextchar == ',':
+        curpos += 1
+        continue
+      else:
+        return False
+    return False
+
   def visitNameexpr(self, ctx: SayScanParser.NameexprContext):
     if node := ctx.QUOTEDSTR():
       self.result.sayer = self.handle_quoted_str(node)
       return
     if node := ctx.NORMALTEXT():
-      self.result.sayer = self.handle_normal_str(node)
+      self.result.sayer = self.trim_text(self.handle_normal_str(node))
       return
     raise RuntimeError("should not happen")
 
@@ -112,9 +148,9 @@ class SayScanner(SayScanVisitor):
   def visitStatusexpr(self, ctx:SayScanParser.StatusexprContext):
     if node := ctx.NORMALTEXT():
       if isinstance(node, list):
-        self.result.expression = [self.handle_normal_str(v) for v in node]
+        self.result.expression = [self.trim_text(self.handle_normal_str(v)) for v in node]
       else:
-        self.result.expression = [self.handle_normal_str(node)]
+        self.result.expression = [self.trim_text(self.handle_normal_str(node))]
 
   def visitContentexpr(self, ctx:SayScanParser.ContentexprContext):
     # 如果第一个终结符是 QUOTEDSTR, 那么我们只处理所有 QUOTEDSTR
@@ -124,8 +160,14 @@ class SayScanner(SayScanVisitor):
       case SayScanParser.QUOTEDSTR:
         for c in ctx.QUOTEDSTR():
           curresult = self.handle_quoted_str(c)
+          # 检查该内容是否需要在后面加空格
+          if (self.should_add_whitespace_after_quoted_str(curresult.end)):
+            curresult.flag_append_space = True
           self.result.content.append(curresult)
-          self.result.is_content_quoted = True
+        # 内容的最后一项后面肯定不加空格
+        if len(self.result.content) > 0:
+          self.result.content[-1].flag_append_space = False
+        self.result.is_content_quoted = True
       case SayScanParser.NORMALTEXT | SayScanParser.SENTENCESPLITTER:
         curresult = self.collect_all_child(ctx)
         self.result.content.append(curresult)
@@ -158,7 +200,7 @@ def analyze_say_expr(text : str, *, debug : bool = False) -> SayScanResult | Non
     return None
   if debug:
     print(tree.toStringTree())
-  scanner = SayScanner()
+  scanner = SayScanner(text)
   scanner.visit(tree)
   return scanner.result
 
