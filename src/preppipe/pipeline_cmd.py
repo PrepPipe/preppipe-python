511986bea168a69e24e9f284ae1d7cc414d0a0d6
Shengjie Xu <xsj617603321@gmail.com>
2025-10-05 23:06:21 +0800

    Adding starting code for frontend export extension

--- src/preppipe/pipeline_cmd.py
+++ src/preppipe/pipeline_cmd.py
@@ -17,6 +17,7 @@ from .frontend import commanddocs
 from .renpy import passes as renpy_passes
 from .webgal import passes as webgal_passes
 from .frontend.vnmodel import passes as vnparser_passes
+from .frontend import inputexport
 from .transform import passes as vnmodel_transform_passes
 from .analysis import passes as vnmodel_analysis_passes
 from . import testbench
